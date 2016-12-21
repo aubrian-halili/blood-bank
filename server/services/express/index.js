35bf55fb4e6013f205bb3cbe1f85e9a21673c605
Aubrian Halili <aubrian.halili.ags@gmail.com>
2016-12-21 17:17:20 +0800

    Integrated webpack-dev-middleware in Express

--- server/services/express/index.js
+++ server/services/express/index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import path from 'path'
 import cors from 'cors'
 import compression from 'compression'
 import morgan from 'morgan'
@@ -19,7 +20,23 @@ export default (routes) => {
 
   app.use(bodyParser.urlencoded({ extended: false }))
   app.use(bodyParser.json())
+  app.use(express.static(path.join(__dirname, '../../../dist')))
   app.use(routes)
+  app.get('*', function (req, res) {
+    res.sendfile(path.join(__dirname, '../../../dist/index.html'))
+  })
+
+  if (env === 'development') {
+    const webpackDev = require('webpack-dev-middleware')
+    const webpack = require('webpack')
+    const webpackConfig = require('../../../config/webpack.dev')
+
+    app.use(webpackDev(webpack(webpackConfig), {
+      publicPath: '/assets/',
+      index: 'index.html'
+    }))
+  }
+
   app.use(queryErrorHandler())
   app.use(bodyErrorHandler())
 
